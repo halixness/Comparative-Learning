different lr, fixed regularizer loss computation, larger HyperMLP

main.py
@@ -19,7 +19,7 @@ from dataset import *
 from models import *
 
 random.seed(1337)
-device = "cuda:1" if torch.cuda.is_available() else "cpu"
+device = "cuda" if torch.cuda.is_available() else "cpu"
 wandb_run = None
 
 def my_train_clip_encoder(dt, model, attr, lesson, memory, epoch):
@@ -27,7 +27,14 @@ def my_train_clip_encoder(dt, model, attr, lesson, memory, epoch):
 	# get model
 	clip_model, clip_preprocess = clip.load("ViT-B/32", device=device)
 	
-	optimizer = optim.Adam(model.parameters(), lr=lr)
+	# It is more stable
+	optimizer = optim.Adam([
+		# model.filter
+		# model.embedding
+		{'params': model.encoder.parameters(), 'lr': 1e-4}
+	], lr=1e-3)
+	# optimizer = optim.Adam(model.parameters(), lr=1e-4)
+
 	model.train()
 
 	loss_sim = None
@@ -37,9 +44,9 @@ def my_train_clip_encoder(dt, model, attr, lesson, memory, epoch):
 	
 	centroid_sim = torch.rand(1, latent_dim).to(device)
 
-	while ct < 1:
+	while ct <= lesson_iterations:
 		ct += 1
-		progressbar = tqdm(range(100))
+		progressbar = tqdm(range(200))
 		for i in progressbar:
 			# Get Inputs: sim_batch, (sim_batch, 4, 128, 128)
 			base_name_sim, images_sim = dt.get_better_similar(attr, lesson)
@@ -48,7 +55,7 @@ def my_train_clip_encoder(dt, model, attr, lesson, memory, epoch):
 				emb = clip_model.encode_image(images_sim).float() # B, 512
 
 			# run similar model
-			z_sim, _ = model(lesson, emb)
+			z_sim = model(lesson, emb)
 			centroid_sim = centroid_sim.detach()
 			centroid_sim, loss_sim = get_sim_loss(torch.vstack((z_sim, centroid_sim)))
 
@@ -59,7 +66,7 @@ def my_train_clip_encoder(dt, model, attr, lesson, memory, epoch):
 				emb = clip_model.encode_image(images_dif).float() # B, 512
 
 			# run difference model
-			z_dif, _ = model(lesson, emb)
+			z_dif = model(lesson, emb)
 			loss_dif = get_sim_not_loss(centroid_sim, z_dif)
 
 			# compute loss
@@ -71,7 +78,9 @@ def my_train_clip_encoder(dt, model, attr, lesson, memory, epoch):
 			log = {
 				"train/loss": loss.detach().item(),
 				"train/loss_sim": loss_sim.detach().item(),
-				"train/loss_dif": loss_dif.detach().item()
+				"train/loss_dif": loss_dif.detach().item(),
+				"epoch": epoch,
+				"centroid": torch.mean(centroid_sim)
 			}
 
 			if len(memory.keys()) > 0:  # hyper-output regularization
@@ -85,6 +94,7 @@ def my_train_clip_encoder(dt, model, attr, lesson, memory, epoch):
 			loss.backward()
 			optimizer.step()
 			if wandb_run: wandb_run.log(log)
+			else: print(log)
 
 		print('[', ct, ']', loss.detach().item(), loss_sim.detach().item(),
 				loss_dif.detach().item())
@@ -92,8 +102,8 @@ def my_train_clip_encoder(dt, model, attr, lesson, memory, epoch):
 	############ save model #########
 	with torch.no_grad():
 		memory[lesson] = {"centroid": centroid_sim}
-		memory[lesson]["params"] = model.get_weights(lesson)
-	return model
+		memory[lesson]["params"] = model.get_weights(lesson) # (L)
+	return model, memory
 
 def compute_regularizer_loss(model:HyperMem, memory:dict) -> th.Tensor:
 	"""
@@ -104,18 +114,14 @@ def compute_regularizer_loss(model:HyperMem, memory:dict) -> th.Tensor:
 		Outputs:
 			loss:th.Tensor		mean task weight loss
 	"""
-	loss = None
+	# print(f"Computing loss for: {list(memory.keys())}")
+	task_loss = 0
 	for lesson, val in memory.items(): # for each old task
-		old_params = val["params"]
-		new_params = model.get_weights(lesson)
-		task_loss = None
-		for k in old_params.keys(): # for each param
-			loss = F.mse_loss(old_params[k], new_params[k], reduction="sum")
-			if task_loss is None: task_loss = loss
-			else: task_loss += loss
-		if loss is None: loss = task_loss
-		else: loss = torch.hstack((loss, task_loss))
-	return torch.mean(loss)
+		old_params = val["params"] # (L)
+		new_params = model.get_weights(lesson) # (L)
+		# task_loss += (old_params - new_params).pow(2).sum()
+		task_loss += F.huber_loss(new_params, old_params, reduction="sum")
+	return torch.mean(task_loss)
 		
 
 def my_clip_evaluation(in_path, source, model, in_base, types, dic, vocab, memory, epoch):
@@ -158,7 +164,7 @@ def my_clip_evaluation(in_path, source, model, in_base, types, dic, vocab, memor
 					emb = clip_model.encode_image(images).float() # B, 512
 
 				# compute stats
-				z, _ = model(label, emb)
+				z = model(label, emb)
 				z = z.squeeze(0)
 				centroid_i = memory[label]["centroid"]
 				centroid_i = centroid_i.repeat(batch_size_i, 1)
@@ -213,33 +219,33 @@ def my_clip_train(in_path, out_path, model_name, source, in_base,
 
 	# load encoder models from memory
 	model = HyperMem(lm_dim=768, knob_dim=128, input_dim=512, hidden_dim=128, output_dim=latent_dim).to(device)
+	print(f"# Params: {count_parameters(model)}")
+	print(f"LR: {lr}")
 
 	best_nt = 0
 	t_tot = 0
 	memory = {}
-
-	notions = [(k, l) for k in types_learning for l in dic[k]]
-	random.shuffle(notions)
-
 	for i in range(epochs):
-		for tl, vi in notions:  # lesson
-			# Train
-			print("#################### Learning: " + str(i) + " ----- " + str(vi))
-			t_start = time.time()
-			model = my_train_clip_encoder(dt, model, tl, vi, memory, i)
-			t_end = time.time()
-			t_dur = t_end - t_start
-			t_tot += t_dur
-			print("Time: ", t_dur, t_tot)
+		for tl in types_learning:  # attr
+			random.shuffle(dic[tl])
+			for vi in dic[tl]:  # lesson
+				# Train
+				print("#################### Learning: " + str(i) + " ----- " + str(vi))
+				t_start = time.time()
+				model, memory = my_train_clip_encoder(dt, model, tl, vi, memory, i)
+				t_end = time.time()
+				t_dur = t_end - t_start
+				t_tot += t_dur
+				print("Time: ", t_dur, t_tot)
 
-			# Evaluate
-			top_nt = my_clip_evaluation(in_path, 'novel_test/', model,
-							bsn_novel_test_1, ['rgba'], dic_train, vocab, memory, i)
-			if top_nt > best_nt:
-				best_nt = top_nt
-				print("++++++++++++++ BEST NT: " + str(best_nt))
-				# with open(os.path.join(out_path, model_name), 'wb') as handle:
-				#	pickle.dump(memory, handle, protocol=pickle.HIGHEST_PROTOCOL)
+				# Evaluate
+				top_nt = my_clip_evaluation(in_path, 'novel_test/', model,
+								bsn_novel_test_1, ['rgba'], dic_train, vocab, memory, i)
+				if top_nt > best_nt:
+					best_nt = top_nt
+					print("++++++++++++++ BEST NT: " + str(best_nt))
+					# with open(os.path.join(out_path, model_name), 'wb') as handle:
+					#	pickle.dump(memory, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 if __name__ == "__main__":
@@ -252,22 +258,31 @@ if __name__ == "__main__":
 				help='Best model memory to be saved file name', required=False)
 	argparser.add_argument('--pre_train', '-p', default=None,
 				help='Pretrained model import name (saved in outpath)', required=False)
+	argparser.add_argument('--wandb', '-w', default=False, type=bool,
+				help='Enable wandb')
+	argparser.add_argument('--run_name', '-r', default="hypernet", type=str,
+				help='Wandb run name')
+	argparser.add_argument('--device', '-d', default="cuda", type=str,
+				help='Device')
+	argparser.add_argument('--lesson_iterations', '-ct', default=4, type=int,
+				help='Iterations per concept')
 	args = argparser.parse_args()
+	
+	lesson_iterations = args.lesson_iterations
 
-	"""
-	wandb.login()
-	config = {
-		"lr": lr,
-		"sim_batch": sim_batch,
-		"gen_batch": gen_batch,
-		"epochs": epochs,
-		"batch_size": batch_size,
-		"latent_dim": latent_dim,
-		"beta_reg": beta_reg
-	}
-	wandb_run = wandb.init(name="hypernet_regularized_shuffled", project="hypernet-concept-learning", config=config)
-	"""
-
+	if args.wandb:
+		wandb.login()
+		config = {
+			"lr": lr,
+			"sim_batch": sim_batch,
+			"gen_batch": gen_batch,
+			"epochs": epochs,
+			"batch_size": batch_size,
+			"latent_dim": latent_dim,
+			"beta_reg": beta_reg
+		}
+		wandb_run = wandb.init(name=args.run_name, project="hypernet-concept-learning", config=config)
+		
 	my_clip_train(args.in_path, args.out_path, args.model_name,
 				'novel_train/', bn_n_train, ['rgba'], dic_train, vocabs, args.pre_train)
 

models.py
@@ -18,6 +18,8 @@ from transformers import BertTokenizer, BertModel
 from config import *
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
 class CLIP_AE_Encode(nn.Module):
 	def __init__(self, hidden_dim, latent_dim, isAE=False):
@@ -62,7 +64,11 @@ class HyperMLP(Model):
         self.in_dim = input_dim
         self.out_dim = output_dim
         self.bias = bias
-        self.ff = nn.Linear(knob_dim, input_dim*output_dim)
+        self.ff = nn.Sequential(
+            nn.Linear(knob_dim, input_dim),
+            nn.Linear(input_dim, output_dim),
+            nn.Linear(output_dim, input_dim*output_dim)
+        )
         if self.bias: self.b = nn.Parameter(th.zeros((output_dim)))
         self.apply(self._init_weights)
 
@@ -73,8 +79,8 @@ class HyperMLP(Model):
             Outputs:
                 w:th.Tensor             predicted MLP weights (1, in_dim*out_dim)
         """
-        w = [self.ff(k)]
-        if self.bias: w.append(self.b) 
+        w = self.ff(k).view(-1)
+        if self.bias: w = torch.hstack((w, self.b.view(-1))) 
         return w
 
     def forward(self, k:th.Tensor, x:th.Tensor) -> th.tensor:
@@ -85,7 +91,7 @@ class HyperMLP(Model):
             Outputs:
                 h:th.Tensor             encoded examples of the form (B, H')
         """
-        W = self.ff(k).reshape((self.out_dim, self.in_dim)) # H', H
+        W = self.ff(k).view(self.out_dim, self.in_dim) # H', H
         h = (x @ th.t(W)) # B, H'
         if self.bias: h += self.b.repeat(h.shape[0], 1)
         return h
@@ -95,9 +101,14 @@ class HyperEncoder(Model):
 
     def __init__(self, knob_dim:int=128, input_dim:int=512, hidden_dim:int=128, output_dim:int=16):
         super(HyperEncoder, self).__init__()
-        self.hyper_encoder_1 = HyperMLP(knob_dim=knob_dim, input_dim=input_dim, output_dim=hidden_dim) # H -> 128
-        self.hyper_encoder_2 = HyperMLP(knob_dim=knob_dim, input_dim=hidden_dim, output_dim=output_dim) # 128 -> 16
+        self.layers = nn.Sequential(
+            HyperMLP(knob_dim=knob_dim, input_dim=input_dim, output_dim=hidden_dim), # H -> 128
+            HyperMLP(knob_dim=knob_dim, input_dim=128, output_dim=output_dim), # 128 -> 16
+        )
         self.apply(self._init_weights)
+
+    def get_weights(self, notion:th.Tensor) -> th.Tensor:
+        return torch.hstack([layer.get_weights(notion) for layer in self.layers])
 
     def forward(self, notion:th.Tensor, x:th.Tensor) -> th.Tensor:
         """
@@ -107,8 +118,8 @@ class HyperEncoder(Model):
             Outputs:
                 h:th.Tensor             encoded examples in the notion's conceptual space
         """
-        x = F.relu(self.hyper_encoder_1(notion, x))
-        return self.hyper_encoder_2(notion, x)
+        for layer in self.layers: x = F.relu(layer(notion, x))
+        return x
 
 
 class HyperMem(Model):
@@ -125,10 +136,11 @@ class HyperMem(Model):
         """
         self._d = nn.Parameter(th.empty(0))
         self._d.requires_grad = False
+
         self.filter = nn.Linear(in_features=knob_dim, out_features=input_dim)
-        self.centroid = nn.Linear(in_features=knob_dim, out_features=input_dim)
         self.embedding = nn.Sequential(nn.Linear(lm_dim, lm_dim//2), nn.Linear(lm_dim//2, knob_dim))
         self.encoder = HyperEncoder(knob_dim=knob_dim, input_dim=input_dim, hidden_dim=hidden_dim, output_dim=output_dim)
+        
         self.bert_tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
         self.bert = BertModel.from_pretrained("bert-base-uncased")
         for name, param in self.bert.named_parameters(): param.requires_grad = False
@@ -148,16 +160,9 @@ class HyperMem(Model):
             e_notion = self.bert(t_notion.input_ids).last_hidden_state[:, 0]
             k = F.relu(self.embedding(e_notion)) # 1, 128
         # HyperNet
-        w_filt = self.filter(k)
-        w_enc1, bias_enc1 = self.encoder.hyper_encoder_1.get_weights(k)
-        w_enc2, bias_enc2 = self.encoder.hyper_encoder_2.get_weights(k)
-        return {
-            "filter": w_filt,
-            "encoder1": w_enc1,
-            "bias_enc1": bias_enc1,
-            "encoder2": w_enc2,
-            "bias_enc2": bias_enc2,
-        }
+        w_filt = self.filter(k).view(-1)
+        w_enc = self.encoder.get_weights(k)
+        return torch.hstack((w_filt, w_enc))
 
     def forward(self, notion:str, x:th.Tensor) -> (th.Tensor, th.Tensor):
         """
@@ -174,10 +179,9 @@ class HyperMem(Model):
             e_notion = self.bert(t_notion.input_ids).last_hidden_state[:, 0]
         e_notion = F.relu(self.embedding(e_notion)) # 1, 128
         # Encoding
-        centroid = self.centroid(e_notion)
         filter = self.filter(e_notion)
         h = x * filter # B, 512
         z = self.encoder(e_notion, h)
-        return z, centroid
+        return z
 
 