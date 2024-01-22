fixed computation, DER++ implementation

main.py
@@ -48,8 +48,8 @@ class Buffer:
 		self.beta = beta
 
 	def get_sample(self) -> object:
-		if len(self.data) == self.size:
-			idx = int(random.uniform(0, self.size-1))
+		if len(self.data) > 1:
+			idx = int(random.uniform(0, len(self.data)-1))
 			return self.data[idx]
 		else: return None
 		
@@ -76,7 +76,7 @@ def my_train_clip_encoder(dt, model, attr, lesson, memory, epoch, buffer):
 	optimizer = optim.Adam([
 		# model.filter
 		# model.embedding
-		{'params': model.encoder.parameters(), 'lr': 1e-4}
+		{'params': model.encoder.parameters(), 'lr': lr}
 	], lr=1e-3)
 	# optimizer = optim.Adam(model.parameters(), lr=1e-4)
 
@@ -89,16 +89,18 @@ def my_train_clip_encoder(dt, model, attr, lesson, memory, epoch, buffer):
 
 	while loss > 0.008:
 		ct += 1
-		if ct > args.lesson_iterations:
+		if ct >= args.lesson_iterations:
 			break
 		progressbar = tqdm(range(200))
 		for i in progressbar:
+
+			optimizer.zero_grad()
 			
 			# Get Inputs: sim_batch, (sim_batch, 4, 128, 128)
 			base_name_sim, images_sim = dt.get_better_similar(attr, lesson)
 			images_sim = images_sim.to(device)
 			with torch.no_grad():
-				sim_emb = clip_model.encode_image(images_sim).float() # B, 512
+				sim_emb = clip_model.encode_image(images_sim).float().detach() # B, 512
 				
 			# run similar model
 			z_sim, centroid_sim = model(lesson, sim_emb)
@@ -109,32 +111,35 @@ def my_train_clip_encoder(dt, model, attr, lesson, memory, epoch, buffer):
 			base_name_dif, images_dif = dt.get_better_similar_not(attr, lesson)
 			images_dif = images_dif.to(device)
 			with torch.no_grad():
-				dif_emb = clip_model.encode_image(images_dif).float() # B, 512
+				dif_emb = clip_model.encode_image(images_dif).float().detach() # B, 512
 				
 			# run difference model
 			z_dif, _ = model(lesson, dif_emb)
 			loss_dif = get_sim_not_loss(centroid_sim, z_dif)
 
-			# Normal loss
+			# Dark Experience Replay (++)
+			sample1 = buffer.get_sample()
+			sample2 = buffer.get_sample()
+			reg = None
+			if sample1 and sample2:
+				# Component 1: matching the logits
+				n1_z_sim, _ = model(sample1["x_lesson"], sample1["x_sim_emb"])
+				n1_z_dif, _ = model(sample1["x_lesson"], sample1["x_dif_emb"])
+				reg_loss1 = buffer.alpha * (F.mse_loss(n1_z_sim, sample1["z_sim"]) + F.mse_loss(n1_z_dif, sample1["z_dif"]))
+				# Component 2: matching the labels (but it's unsupervised)
+				n2_z_sim, n2_centroid = model(sample1["x_lesson"], sample1["x_sim_emb"])
+				n2_z_dif, _ = model(sample1["x_lesson"], sample1["x_dif_emb"])
+				n2_centroid = n2_centroid.squeeze(0)
+				reg_loss_sim = h_get_sim_loss(n2_z_sim, n2_centroid)
+				reg_loss_dif = get_sim_not_loss(n2_centroid, n2_z_dif)
+				reg_loss2 = buffer.beta * ((reg_loss_sim)**2 + (reg_loss_dif-1)**2)
+				# DER++
+				reg =  reg_loss1 + reg_loss2
+
+			# Loss
 			loss = (loss_sim)**2 + (loss_dif-1)**2
+			if reg: loss = loss + reg
 
-			# Dark Experience Replay
-			sample = buffer.get_sample()
-			if sample is not None:
-				# component 2
-				z_sim, centroid = model(sample["x_lesson"], sample["x_sim_emb"])
-				z_dif, _ = model(sample["x_lesson"], sample["x_dif_emb"])
-				centroid = centroid_sim.squeeze(0)
-				reg_loss_sim = h_get_sim_loss(z_sim, centroid)
-				reg_loss_dif = get_sim_not_loss(centroid, z_dif)
-				reg = (reg_loss_sim)**2 + (reg_loss_dif-1)**2
-				loss = loss + buffer.alpha * reg
-
-			# Backprop
-			optimizer.zero_grad()
-			loss.backward()
-			optimizer.step()
-			# Log
 			log = {
 				"train/loss": loss.detach().item(),
 				"train/loss_sim": loss_sim.detach().item(),
@@ -142,16 +147,24 @@ def my_train_clip_encoder(dt, model, attr, lesson, memory, epoch, buffer):
 				"epoch": epoch,
 				"centroid": torch.mean(centroid_sim)
 			}
+			if reg is not None: log["train/regularizer"] = reg
+
+			# Backprop
+			loss.backward()
+			optimizer.step()
+			# Log
 			progressbar.set_description(f"loss: {loss.item():.2f}")
 			if wandb_run: wandb_run.log(log)
 			else: print(log)
+			
 			# Update reservoir
 			with torch.no_grad():
 				buffer.add_sample({
 					"x_lesson": lesson,
-					"x_sim_emb": sim_emb,
-					"x_dif_emb": dif_emb,
-					# "z": model.get_weights(lesson),
+					"x_sim_emb": sim_emb.detach(),
+					"x_dif_emb": dif_emb.detach(),
+					"z_sim": z_sim.detach(),
+					"z_dif": z_dif.detach(),
 				})
 
 		print('[', ct, ']', loss.detach().item(), loss_sim.detach().item(),
@@ -261,8 +274,8 @@ def my_clip_train(in_path, out_path, model_name, source, in_base,
 	print(f"[-] #params: {count_parameters(model)}")
 
 	# Define a buffer
-	alpha, beta = hyperparams
-	buffer = Buffer(alpha=alpha, beta=beta, size=200)
+	alpha, beta, buffer_size = hyperparams
+	buffer = Buffer(alpha=alpha, beta=beta, size=buffer_size)
 
 	best_nt = 0
 	t_tot = 0
@@ -333,6 +346,6 @@ if __name__ == "__main__":
 		wandb_run = wandb.init(name=args.run_name, project="hypernet-concept-learning", config=config)
 		
 	my_clip_train(args.in_path, args.out_path, args.model_name,
-				'novel_train/', bn_n_train, ['rgba'], dic_train, vocabs, args.pre_train, (args.alpha, args.beta))
+				'novel_train/', bn_n_train, ['rgba'], dic_train, vocabs, args.pre_train, (args.alpha, args.beta, args.buffer_size))
 
 	

models/novel/models.py
@@ -36,18 +36,13 @@ class Model(nn.Module):
             module.bias.data.zero_()
             module.weight.data.fill_(1.0)
 
-
 class HyperMLP(Model):
     def __init__(self, knob_dim:int, input_dim:int, output_dim:int, bias:bool=True):
         super(HyperMLP, self).__init__()
         self.in_dim = input_dim
         self.out_dim = output_dim
         self.bias = bias
-        hidden_dim = (input_dim*output_dim) // 32
-        self.ff = nn.Sequential(
-            nn.Linear(knob_dim, hidden_dim),
-            nn.Linear(hidden_dim, input_dim*output_dim)
-        )
+        self.ff = nn.Linear(knob_dim, input_dim*output_dim)
         if self.bias: self.b = nn.Linear(knob_dim, output_dim)
         self.apply(self._init_weights)
 
@@ -70,19 +65,18 @@ class HyperMLP(Model):
             Outputs:
                 h:th.Tensor             encoded examples of the form (B, H')
         """
-        W = self.ff(k).view(self.out_dim, self.in_dim) # H', H
-        h = (x @ th.t(W)) # B, H'
-        if self.bias: h += self.b(k).repeat(h.shape[0], 1) # (B, H')
+        k = k.view(-1)
+        w = self.ff(k).view(self.out_dim, self.in_dim)
+        b = self.b(k).view(-1)
+        h = F.linear(x, weight=w, bias=b)
         return h
 
 class HyperEncoder(Model):
 
     def __init__(self, knob_dim:int=128, input_dim:int=512, hidden_dim:int=128, output_dim:int=16):
         super(HyperEncoder, self).__init__()
-        self.layers = nn.Sequential(
-            HyperMLP(knob_dim=knob_dim, input_dim=input_dim, output_dim=hidden_dim),
-            HyperMLP(knob_dim=knob_dim, input_dim=hidden_dim, output_dim=latent_dim),
-        )
+        self.down_mlp = HyperMLP(knob_dim=knob_dim, input_dim=input_dim, output_dim=hidden_dim)
+        self.up_mlp = HyperMLP(knob_dim=knob_dim, input_dim=hidden_dim, output_dim=latent_dim)
         self.apply(self._init_weights)
 
     def get_weights(self, notion:th.Tensor) -> th.Tensor:
@@ -96,9 +90,8 @@ class HyperEncoder(Model):
             Outputs:
                 h:th.Tensor             encoded examples in the notion's conceptual space
         """
-        for l in self.layers[:-1]: x = F.relu(l(notion, x))
-        return self.layers[-1](notion, x)
-
+        x = F.gelu(self.down_mlp(notion, x))
+        return self.up_mlp(notion, x)
 
 class HyperMem(Model):
     
@@ -115,9 +108,9 @@ class HyperMem(Model):
         self._d = nn.Parameter(th.empty(0))
         self._d.requires_grad = False
 
-        self.filter = nn.Sequential(nn.Linear(in_features=knob_dim, out_features=input_dim), nn.ReLU())
-        self.centroid = nn.Sequential(nn.Linear(in_features=knob_dim, out_features=latent_dim), nn.ReLU())
-        self.embedding = nn.Sequential(nn.Linear(lm_dim, lm_dim//2), nn.Linear(lm_dim//2, knob_dim), nn.ReLU())
+        self.filter = nn.Linear(in_features=knob_dim, out_features=input_dim) # from baseline
+        self.centroid = nn.Linear(in_features=knob_dim, out_features=latent_dim) # from baseline
+        self.embedding = nn.Sequential(nn.Linear(lm_dim, lm_dim), nn.Linear(lm_dim, knob_dim), nn.ReLU()) # from paper
         self.encoder = HyperEncoder(knob_dim=knob_dim, input_dim=input_dim, hidden_dim=hidden_dim, output_dim=output_dim)
         
         self.bert_tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
@@ -156,8 +149,8 @@ class HyperMem(Model):
         # Notion embedding
         with th.no_grad():
             t_notion = self.bert_tokenizer(notion, return_tensors="pt").to(self._d.device)
-            e_notion = self.bert(t_notion.input_ids).last_hidden_state[:, 0]
-        e_notion = F.gelu(self.embedding(e_notion)) # 1, 128
+            e_notion = self.bert(t_notion.input_ids).last_hidden_state[:, 0].detach()
+        e_notion = self.embedding(e_notion) # 1, 128
         # Encoding
         f = self.filter(e_notion)
         c = self.centroid(e_notion)