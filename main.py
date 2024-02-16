15ba168a4a79ccc52ac2ddcf983f9b099803b1b6
Filippo <filippo99merlo@gmail.com>
2024-02-16 15:39:52 +0100

    Fix model saving for different lessons

--- main.py
+++ main.py
@@ -174,8 +174,11 @@ def my_train_clip_encoder(resume_iter, rank, training_data, n_split, memory, in_
 		# So for each lesson switch -> save model
 		if (rank == 0) and (lesson != previous_lesson):
 			with torch.no_grad():
-				memory[lesson] = True
-				torch.save(model.state_dict(), os.path.join(out_path,"checkpoints", f"hypernet_learned{len(memory.keys())}_{int(round(datetime.now().timestamp()))}.pth"))
+				try:
+					memory[lesson] = True
+					torch.save(model.state_dict(), os.path.join(out_path,"checkpoints", f"hypernet_learned{len(memory.keys())}_{int(round(datetime.now().timestamp()))}.pth"))
+				except:
+					print(f"[-] Error saving model for lesson: {lesson}")
 		previous_lesson = lesson
 		i += 1
 		
